f3de21476acca4ba649e7045c14b4f6257753df2
Jimmy <jimmywu126@gmail.com>
2014-02-21 19:41:33 -0800

    Implemented gini index computation, both nationwide and weighted average over zip codes.

--- code/gini.py
+++ code/gini.py
@@ -5,6 +5,7 @@ Classes are strings."""
 
 import fileinput
 import csv
+from collections import *
 
 (
     CMTE_ID, AMNDT_IND, RPT_TP, TRANSACTION_PGI, IMAGE_NUM, TRANSACTION_TP,
@@ -20,6 +21,7 @@ CANDIDATES = {
 
 ############### Set up variables
 # TODO: declare datastructures
+contributions_by_zip_and_candidate = {}
 
 ############### Read through files
 for row in csv.reader(fileinput.input(), delimiter='|'):
@@ -32,12 +34,37 @@ for row in csv.reader(fileinput.input(), delimiter='|'):
     ###
     # TODO: save information to calculate Gini Index
     ##/
+    contributions_by_zip_and_candidate.setdefault(zip_code, defaultdict(int))[candidate_name] += 1
 
 ###
 # TODO: calculate the values below:
 gini = 0  # current Gini Index using candidate name as the class
 split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
 ##/
+def compute_gini_index(values):
+    print values
+    total = sum(values)
+    gini_index = 1 - sum([ (float(value) / float(total)) ** 2 for value in values])
+    return gini_index
+
+# Gini index for all zip codes
+contributions_by_candidate = defaultdict(int)
+for contributions_in_zip in contributions_by_zip_and_candidate.itervalues():
+    for candidate, contributions in contributions_in_zip.iteritems():
+        contributions_by_candidate[candidate] += contributions
+gini = compute_gini_index(contributions_by_candidate.values())
+total_contributions = float(sum(contributions_by_candidate.itervalues()))
+
+# Average Gini index over zip codes, weighted by number of records in that zip code
+for contributions_by_candidate_in_zip in contributions_by_zip_and_candidate.itervalues():
+    split_gini += (float(sum(contributions_by_candidate_in_zip.values())) / total_contributions) * compute_gini_index(contributions_by_candidate_in_zip.values())
+
 
 print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
+
+"""
+OUTPUT:
+Gini Index: 0.487709185938
+Gini Index after split: 0.414381001914
+"""
