4c8997910d631646ae63f8a365d72c86fcd7cfe3
Jimmy <jimmywu126@gmail.com>
2014-02-28 15:45:51 -0800

    Simplified constants. Added hidden layer error function.

--- code/neural_network_back_propagation.py
+++ code/neural_network_back_propagation.py
@@ -6,17 +6,16 @@ from lecture.
 # Learning rate
 l = 10
 
-nodes_and_outputs = [
-	# (1, 1),
-	# (2, 2),
-	(3, 0.7311),
-	(4, 0.0179),
-	(5, 0.9933),
-	(6, 0.8387),
-]
-output_for_node = {node: output for node, output in nodes_and_outputs}
+output_for_node = {
+	# 1: 1,
+	# 2: 2,
+	3: 0.7311,
+	4: 0.0179,
+	5: 0.9933,
+	6: 0.8387,
+}
 
-weights_between_nodes = {
+weight_between_nodes = {
 	(1, 3): -3,
 	(1, 4): 2,
 	(1, 5): 4,
@@ -28,17 +27,24 @@ weights_between_nodes = {
 	(5, 6): 1.5,
 }
 
-def error(actual, expected):
+def output_error(actual, expected):
 	"""
 	Calculates the error in a node's output.
 	Intuitively, error is
-		Δf(x)f'(x) = [y - f(x)][1 - f(x)]f(x)
+		[y - f(x)]f'(x) = [y - f(x)][1 - f(x)]f(x)
 	where
 		y := expected output
 		f(x) := actual output of logistic/sigmoid function on input x
 	"""
 	actual, expected = float(actual), float(expected)
 	return (expected - actual) * (1 - actual) * actual
+
+def hidden_layer_error(output_j, errors_k_and_weights_jk):
+	"""
+	Calculates the error of a node in a hidden layer
+	that has directed edges to nodes k.
+	"""
+	return output_j * (1 - output_j) * sum([error_k * weight_jk for error_k, weight_jk in errors_k_and_weights_jk])
 
 def new_weight(weight_ij, error_j, output_i, learning_rate=l):
 	"""
